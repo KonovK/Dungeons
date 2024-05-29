60148b1fa541b11f92ca6fae6e298961488b90e8
KonovK <konov.ki@yandex.ru>
2024-05-29 20:05:16 +0300

    Начало кода

---
+++ src/heroes/Player.java
@@ -0,0 +1,14 @@
+package heroes;
+
+public class Player {
+
+    int heals;
+    String bag;
+    String position;
+
+    public void Game(int heals, String bag, String position) {
+        this.heals = heals;
+        this.bag = bag;
+        this.position = position;
+    }
+}

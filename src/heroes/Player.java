b1bc8c791d1a26b9cb3b9aac98e2f5b384d9877f
KonovK <konov.ki@yandex.ru>
2024-05-29 20:38:17 +0300

    Начало кода

--- src/heroes/Player.java
+++ src/heroes/Player.java
@@ -2,13 +2,37 @@ package heroes;
 
 public class Player {
 
-    int heals;
-    String bag;
-    String position;
+    private int heals;
+    private String bag;
+    private String position;
 
-    public void Game(int heals, String bag, String position) {
+    public Player(int heals, String bag, String position) {
         this.heals = heals;
         this.bag = bag;
+        this.position = position;
+    }
+
+    public int getHeals() {
+        return heals;
+    }
+
+    public String getBag() {
+        return bag;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setHeals(int heals) {
+        this.heals = heals;
+    }
+
+    public void setBag(String bag) {
+        this.bag = bag;
+    }
+
+    public void setPosition(String position) {
         this.position = position;
     }
 }

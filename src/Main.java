b9e5fd525e6bccecd036650e6a4648d48627fa41
KonovK <konov.ki@yandex.ru>
2024-06-01 10:20:15 +0300

    Начало кода

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import dungeons.Dungeon;
 import game.Game;
 
 public class Main {
     public static void main(String[] args) {
         Game.start();
+        Dungeon dungeon = new Dungeon();
+        System.out.println(dungeon);
     }
 }
--- src/dungeons/Dungeon.java
+++ src/dungeons/Dungeon.java
@@ -1,4 +1,14 @@
 package dungeons;
 
 public class Dungeon {
+
+    private int[][] matrix = new int[10][10];
+
+    public Dungeon() {
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; i++){
+                matrix[i][j] = 1;
+            }
+        }
+    }
 }
